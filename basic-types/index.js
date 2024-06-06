b7f4bfde5241726f732f719ad1a29647c9d7ac74
Paul Drahun <drahunpavel@gmail.com>
2024-06-07 00:19:13 +0300

    added basic-types

---
+++ basic-types/index.js
@@ -0,0 +1,9 @@
+//* number
+var a_n = 1;
+var b_n = -10;
+var c_n = 3.5;
+var d_n = 10000;
+//* string
+var a_s = 'test';
+//* boolean
+var a_b = false;
---
+++ basic-types/index.ts
@@ -0,0 +1,12 @@
+//* number
+const a_n: number = 1;
+const b_n: number = -10
+const c_n: number = 3.5
+const d_n: number = 10_000
+
+//* string
+const a_s: string = 'test'
+
+//* boolean
+const a_b: boolean = false
+
